34284e14955188a6c0dc65033d991fd5d0c231bd
Nelson <amigoscode@gmail.com>
2019-11-03 22:01:46 +0000

    end of section 2

---
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
+
+}
---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,21 @@
+package com.example.demo.student;
+
+public class Student {
+
+    private final Integer studentId;
+    private final String studentName;
+
+    public Student(Integer studentId,
+                   String studentName) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+    }
+
+    public Integer getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+}
