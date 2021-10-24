1acea909fc0e46e22955c4cb7f7e152ce21a1f0d
Su, Chen <>
2021-10-24 21:47:32 +0800

    add unit test

---
+++ src/test/java/com/example/demo/student/StudentControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.demo.student;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StudentControllerTest {
+
+    @Test
+    public void testGetStudent() {
+        StudentController controller = new StudentController();
+        Student student = controller.getStudent(1);
+        Assertions.assertEquals("James Bond1", student.getStudentName());
+    }
+}
